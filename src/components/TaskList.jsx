82b5bddb30393a1e1254389f061dfaf61b9ed61b
anudharmik <24dharmika@rbunagpur.in>
2026-05-28 20:47:36 +0530

    Improve code efficiency and cleanliness by removing .sort() for better understanding

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,28 +2,13 @@ import TaskItem from "./TaskItem";
 
 export default function TaskList({tasks,goalId,toggleTask,deleteTask,updateTask,darkMode,aiInsights = { focusToday: [] }}){
     const filteredTasks=tasks.filter(task=>task.goal_id===goalId);
-    const sortedTasks = [...filteredTasks].sort((a, b) => {
-
-  const aFocus =
-    aiInsights.focusToday?.some(
-      title =>
-        title.toLowerCase().trim() ===
-        a.title.toLowerCase().trim()
-    );
-
-  const bFocus =
-    aiInsights.focusToday?.some(
-      title =>
-        title.toLowerCase().trim() ===
-        b.title.toLowerCase().trim()
-    );
-
-  // Focus tasks first
-  if (aFocus && !bFocus) return -1;
-  if (!aFocus && bFocus) return 1;
-
-  return 0;
-});
+    const checkFocus = (task) => 
+      aiInsights.focusToday?.some(
+        title => title.toLowerCase().trim() === task.title.toLowerCase().trim()
+      );
+    const focusTasks = filteredTasks.filter(task => checkFocus(task));
+    const regularTasks = filteredTasks.filter(task => !checkFocus(task));
+    const sortedTasks = [...focusTasks, ...regularTasks];
 
     
     return (
